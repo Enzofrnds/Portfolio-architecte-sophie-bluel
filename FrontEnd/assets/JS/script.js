71a0c2d7d08305f19ef238c6aae9f7a6149139cf
Enzofrnds <enzofernandes3@outlook.fr>
2026-03-20 11:47:41 +0100

    feat: filtrage des travaux par categorie

--- FrontEnd/assets/JS/script.js
+++ FrontEnd/assets/JS/script.js
@@ -15,4 +15,36 @@ async function generateWorks() {
 
 generateWorks();
 
+async function fliterByCategory() {
+    const reponse = await fetch(`http://localhost:5678/api/works`);
+    const work = await reponse.json();
+    const btns = document.querySelectorAll('.btn-filter');
+    btns.forEach(btn => {
+        btn.addEventListener('click', () => {
+            btns.forEach(btn => btn.classList.remove('active'));
+            btn.classList.add('active');
 
+            const category = btn.innerText;
+
+            if (category === 'Tous'){
+                document.querySelector('.gallery').innerHTML = '';
+                console.log(work);
+                generateWorks();
+            }
+            else{
+                const filteredWorks = work.filter(work => work.category.name === category);
+                document.querySelector('.gallery').innerHTML = '';
+                filteredWorks.forEach(work => {
+                    const workElement = document.createElement('figure');
+                    workElement.innerHTML = `
+                        <img src="${work.imageUrl}" alt="${work.title}">
+                        <figcaption>${work.title}</figcaption>
+                    `;
+                    document.querySelector('.gallery').appendChild(workElement);
+                });
+            }
+        })
+    })
+}
+
+fliterByCategory();
