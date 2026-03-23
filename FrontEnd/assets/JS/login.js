67c3e2c1ed8e43f8be1e4b5735a6b86535ce7416
Enzofrnds <enzofernandes3@outlook.fr>
2026-03-23 15:40:33 +0100

    feat: authentification utilisateur

---
+++ FrontEnd/assets/JS/login.js
@@ -0,0 +1,24 @@
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const btn = document.getElementById("btn-login");
+
+btn.addEventListener("click", async (event) => {
+    event.preventDefault();
+    const response = await fetch('http://localhost:5678/api/users/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            email: email.value,
+            password: password.value,
+        })
+    });
+    if (response.ok) {
+        const data = await response.json();
+        localStorage.setItem('token', data.token);
+        window.location.href = 'index.html';
+    } else {
+        alert("Erreur dans l’identifiant ou le mot de passe");
+    }
+});
